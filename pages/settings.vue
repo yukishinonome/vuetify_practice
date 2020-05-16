7aef16108e863bd6bb932e1c179782d1ee3535b8
yukishinonome <yuki.yamakoshi0126@gmail.com>
2020-05-17 00:34:37 +0900

    設定ページにカードを追加

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,5 +1,19 @@
 <template>
   <v-layout>
-    <v-flex xs1></v-flex>
+    <v-flex>
+      <v-card class="mx-auto" max-width="344" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <!-- <div class="overline mb-4">OVERLINE</div> -->
+            <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
+            <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
+
+<script>
+export default {}
+</script>
